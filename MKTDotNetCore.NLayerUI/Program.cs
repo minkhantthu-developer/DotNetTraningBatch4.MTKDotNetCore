362b12ac6253971509ccbaa15ad52240deff0b68
Min Khant Thu <dsalpha3367@gmail.com>
2024-06-24 19:53:03 +0630

    [Complete] N Layer Architecture

---
+++ MKTDotNetCore.NLayerUI/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MKTDotNetCore.RestApiWithNLayer.Feature.Blog;
+
+Console.WriteLine("Hello, World!");
+BL_Blog bl_blog = new BL_Blog();
+var lst =bl_blog.GetBlogs();
+foreach (var item in lst)
+{
+    Console.WriteLine($"BlogID => {item.BlogId}");
+    Console.WriteLine($"BlogTitle => {item.BlogTitle}");
+    Console.WriteLine($"BlogAuthor => {item.BlogAuthor}");
+    Console.WriteLine($"BlogContent => {item.BlogContent}");
+    Console.WriteLine("--------------");
+}
